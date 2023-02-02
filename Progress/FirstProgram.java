64ea53405e0bac19340652733ca8f32dade51a7d
nikhilkammula9 <123388198+nikhilkammula9@users.noreply.github.com>
2023-02-01 22:19:25 -0600

    Create FirstProgram.java

---
+++ Progress/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram //Here Class name should always start with an upper-case letter and must be same as the filename
+{
+    public static void main(String[] args) //consider only strings for this program
+    {
+        System.out.println("Hello World"); // Always end with semi colon
+    }
+}
