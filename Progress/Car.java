7e67ccf0a4b40b6379f57650900c876bd071d612
nikhilkammula9 <123388198+nikhilkammula9@users.noreply.github.com>
2023-02-09 00:25:08 -0600

    Create Car.java

---
+++ Progress/Car.java
@@ -0,0 +1,24 @@
+public class Car {
+    String Model;
+    boolean hasOwner;
+    int Mileage;
+    
+    public Car(String CarModel, boolean CarOwner, int CarMileage) {
+      System.out.println("Constructor invoked!");
+      Model = CarModel;
+      hasOwner = CarOwner;
+      Mileage = CarMileage;
+    }
+    
+    public static void main(String[] args) {
+      System.out.println("Main method started");
+      Car BMW = new Car("BMW x", false, 4000);
+      Car Tesla = new Car("Telsa S", true, 12000);
+      boolean isBMWnew = BMW.Mileage > Tesla.Mileage;
+      int totalMileage = BMW.Mileage + Tesla.Mileage;
+      System.out.println("Two cars I love are " + BMW.Model + " and a " + Tesla.Model);
+      System.out.println("The statement that BMW is an older is: " + isBMWnew);
+      System.out.println("The total age of the cars is: " + totalMileage);
+      System.out.println("Main method finished");
+    }
+  }
