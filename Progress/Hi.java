101798281c9bbdf2fb64dd66725754698bc803d9
nikhilkammula9 <123388198+nikhilkammula9@users.noreply.github.com>
2023-03-02 12:02:52 -0600

    Create Hi.java

---
+++ Progress/Hi.java
@@ -0,0 +1,13 @@
+class Hi {
+  
+  public static void main(String[] args) {
+    
+    for (int Hi = 1; Hi <= 100; Hi++) {
+      
+      System.out.println("i told Hi #" + Hi);
+      
+    }
+    
+  }
+  
+}
