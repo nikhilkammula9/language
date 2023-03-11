2e2e1ba345cb42e2eb536cf136b427b47e384af9
nikhilkammula9 <123388198+nikhilkammula9@users.noreply.github.com>
2023-03-11 13:29:25 -0600

    Create stringMethods.java

---
+++ Progress/stringMethods.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+  
+	public static void main(String[] args) {
+    
+    String str = "Hello, World!";
+    System.out.println(str.length());
+    System.out.println(str.substring(4));
+    System.out.println(str.toUpperCase());
+    System.out.println(str.toLowerCase());
+    System.out.println(str.indexOf("2"));
+  }
+  
+}
